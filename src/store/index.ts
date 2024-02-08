85af3d8e53c83900c3a984ecb04c3da81ab5fbc2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-08 17:42:34 +0900

    feat: Implement tag slice and tags slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import noteSlice from "./notes/note.slice";
 import notesSlice from "./notes/notes.slice";
 import noteStateSlice from "./notes/noteState.slice";
+import tagSlice from "./tags/tag.slice";
+import tagsSlice from "./tags/tags.slice";
 
 export const store = configureStore({
     reducer: {
         noteSlice,
         notesSlice,
         noteStateSlice,
+        tagSlice,
+        tagsSlice,
     },
 });
 
