c718f542ea396d15e1cbf0f7d1de21d674408d53
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-09 15:20:27 +0900

    feat: Implement create tag modal component

---
+++ src/components/Modal/CreateTagModal/CreateTagsModal.tsx
@@ -0,0 +1,52 @@
+import React, { FormEvent, MouseEvent, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../../store";
+import Tag from "./Tag";
+import { CiCircleRemove } from "react-icons/ci";
+import { closeTagCreateModal } from "../../../store/modal/modal.slice";
+import { createTag } from "../../../store/tags/tags.slice";
+
+function CreateTagsModal() {
+
+    const tags = useSelector((state: RootState) => state.tagsSlice);
+    const dispatch = useDispatch();
+
+    const [tagName, setTagName] = useState("");
+
+    const handleBackgroundClick = (e: MouseEvent<HTMLElement>) => {
+        const clickedElement = e.target as HTMLElement;
+        if (clickedElement.classList.contains("edit__background"))
+            handleCloseModal();
+    };
+
+    const handleCloseModal = () => {
+        dispatch(closeTagCreateModal());
+    };
+
+    const handleSubmitTag = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(createTag(tagName));
+        setTagName("");
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTagName(e.target.value);
+    };
+
+    return (
+        <div className="edit__background" onClick={handleBackgroundClick}>
+            <div className="edit__tags">
+                <CiCircleRemove className="edit__delete" onClick={handleCloseModal} />
+                <p className="edit__title">Edit Tags</p>
+                <form className="edit__tags__form" onSubmit={handleSubmitTag}>
+                    <input type="text" name="value" value={tagName} onChange={onChange} placeholder="new tag..." />
+                </form>
+                {tags.map((tag) => (
+                    <Tag key={tag} value={tag} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default CreateTagsModal;
