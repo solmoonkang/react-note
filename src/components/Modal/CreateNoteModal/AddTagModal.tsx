300ff97de5763f5daaf292e1d0e39e8b478373d8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-09 15:10:48 +0900

    feat: Implement add tag modal component

---
+++ src/components/Modal/CreateNoteModal/AddTagModal.tsx
@@ -0,0 +1,61 @@
+import React, { MouseEvent } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../../store";
+import { closeTagAddModal } from "../../../store/modal/modal.slice";
+import { CiCircleRemove } from "react-icons/ci";
+
+interface AddTagModalProps {
+    tags: string[];
+    onChange: (value: string[]) => void;
+}
+
+function AddTagModal({ tags, onChange }: AddTagModalProps) {
+
+    const tagList = useSelector((state: RootState) => state.tagsSlice);
+    const dispatch = useDispatch();
+
+    const handleBackgroundClick = (e: MouseEvent<HTMLElement>) => {
+        const clickedElement = e.target as HTMLElement;
+        if (clickedElement.classList.contains("edit__background")) 
+            handleCloseModal();
+    };
+
+    const handleCloseModal = () => {
+        dispatch(closeTagAddModal());
+    };
+
+    const handleRemoveTag = (value: string) => {
+        if (tags.includes(value)) {
+            onChange(tags.filter((data) => data !== value));
+        }
+    };
+
+    const handleAddTag = (value: string) => {
+        if (!tags.includes(value)) {
+            onChange([...tags, value]);
+        }
+    };
+
+    return (
+        <div className="edit__tag__background" onClick={handleBackgroundClick}>
+            <div className="edit__add__tags">
+                <CiCircleRemove className="edit__delete" onClick={handleCloseModal} />
+                <p className="edit__title">태그 추가하기</p>
+                <div className="edit__add__tag__list">
+                    {tagList.map((value) => (
+                        <div key={value} className="edit__tag">
+                            <p>{value}</p>
+                            {tags.includes(value) ? (
+                                <button onClick={() => handleRemoveTag(value)}>-</button>
+                            ) : (
+                                <button onClick={() => handleAddTag(value)}>+</button>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AddTagModal;
