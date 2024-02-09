8226c1bbbc0d46c69a970d48f8400289f9d975de
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-09 19:44:14 +0900

    feat: Implement modal component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Main from "./components/Main";
 import Nav from "./components/Sidebar/Nav/Nav";
+import { RootState } from "./store";
+import CreateTagsModal from "./components/Modal/CreateTagModal/CreateTagsModal";
+import CreateNoteModal from "./components/Modal/CreateNoteModal/CreateNoteModal";
+import ReadNoteModal from "./components/Modal/ReadNoteModal/ReadNoteModal";
 
 function App() {
+
+    const modal = useSelector((state: RootState) => state.modalSlice);
   
-  return (
-    <div className="App">
-      <Nav />
-      <Main />
-    </div>
-  );
+    return (
+      <div className="App">
+        <Nav />
+        <Main />
+        {modal.tag_create && <CreateTagsModal />}
+        {modal.note_edit && <CreateNoteModal />}
+        {modal.note_read && <ReadNoteModal />}
+      </div>
+    );
 }
 
 export default App;
