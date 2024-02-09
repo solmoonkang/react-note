f411abd94fd4b01be62b0e9a9dbf67e89c8a4183
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-09 15:28:24 +0900

    feat: Import Tag.css

--- src/components/Modal/CreateTagModal/Tag.tsx
+++ src/components/Modal/CreateTagModal/Tag.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { removeTag } from "../../../store/tags/tags.slice";
+
 import { CiEraser } from "react-icons/ci";
+import "./Tag.css";
 
 interface TagProps {
     value: string;
